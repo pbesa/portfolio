0ba30bcdf3375480828815ac4785d472a1b2346d
Paulette <paulette.besa@gmail.comls>
2020-04-29 19:05:40 -0400

    stgo-webiste update

--- stgo-website/js/script.js
+++ stgo-website/js/script.js
@@ -1,26 +1,27 @@
 $(document).ready(function(){
 
-	var close = $('.close-btn'),
+	var close = $('.close-btn, .modal-overlay'),
 		openButton = $('.open-box')
-	
+
 	$.each(openButton, function(index, val){
 		$(this).on('click', function(event){
 			$(this).parent().siblings('.modal-container').addClass('open');
-			close.fadeIn(4000);
 			$("body").css({'overflow': 'hidden', 'height': '100%'});
+			$('.modal-overlay').fadeIn(500);
 			event.preventDefault();
 		});
-		
+
 	close.on('click', function(){
 		$('.modal-container').removeClass('open');
-		close.fadeOut(1000);
+		// close.fadeOut(4000);
 		$("body").css({'overflow': 'auto'});
+		$('.modal-overlay').fadeOut(500);
+		event.preventDefault();
 		});
-		
-		
-		
+
 	});
-		
+
+
 
 
 });
